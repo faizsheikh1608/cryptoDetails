e81db7f89a365a277aa2a91f18a100ed82103a93
Faiz <sheikhfaiz7861@gmail.com>
2024-10-05 17:28:49 +0530

    Deploying build to GitHub Pags

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./component/Body";
 
-const AppLayout = function () {
-  return (
-    <div className="app--body">
-      <Body />
-    </div>
-  );
-};
-
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(<AppLayout />);
+root.render(<Body />);
